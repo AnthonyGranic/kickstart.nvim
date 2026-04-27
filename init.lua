b9bb4a633621bf464e3b5a36029257b349f3d16c
Anthony Granic <granicanthony@gmail.com>
2026-04-27 14:25:41 -0400

    added leader sp for search git files

--- init.lua
+++ init.lua
@@ -425,6 +425,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sp', builtin.git_files, { desc = '[S]earch in Git [P]roject' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
       -- it is better explained there). This allows easily switching between pickers if you prefer using something else!
